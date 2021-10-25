2058e43d189f47756a94930e580882d011245f35
Illubyte <business.illubyte@gmail.com>
2021-10-25 18:21:15 -0400

    Add bare-bones view files for theme and palette

---
+++ src/ui/src/views/ViewPalette.vue
@@ -0,0 +1,22 @@
+
+<template>
+	<div class="palette">
+		Palette view
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue'
+
+export default defineComponent( {
+	setup() {
+
+		return {
+		}
+	},
+} )
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/ui/src/views/ViewTheme.vue
@@ -0,0 +1,22 @@
+
+<template>
+	<div class="theme">
+		Theme view
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue'
+
+export default defineComponent( {
+	setup() {
+
+		return {
+		}
+	},
+} )
+</script>
+
+<style lang="sass" scoped>
+
+</style>
