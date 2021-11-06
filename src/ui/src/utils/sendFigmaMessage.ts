faece089292bb2ebc9c32393948ddb4396d51c61
Illubyte <business.illubyte@gmail.com>
2021-11-06 13:34:40 -0400

    Added warning message when using Vite dev mode

--- src/ui/src/utils/sendFigmaMessage.ts
+++ src/ui/src/utils/sendFigmaMessage.ts
@@ -1,5 +1,8 @@
 import { FigmaMessage, MessageWaitingForResponse } from '../types/FigmaMessage'
 
 export function sendFigmaMessage( message: FigmaMessage | MessageWaitingForResponse ) {
+	if( window.parent === window ) {
+		console.warn( 'Window and parent are same context: postMessage is posting to self. Messages are compatible, however, no information will be saved, read, or otherwise returned from Figma.' )
+	}
 	parent.postMessage( { pluginMessage: message }, '*' )
 }
