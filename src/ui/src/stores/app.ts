d507dcc79ea0b4061722068717a0579417f20a30
Illubyte <business.illubyte@gmail.com>
2021-11-14 13:33:57 -0500

    Tamed an unruly unsetOverlay

--- src/ui/src/stores/app.ts
+++ src/ui/src/stores/app.ts
@@ -41,8 +41,12 @@ export const useAppStore = defineStore( {
 				console.error( `View type of '${ type }' does not exist in View Map.` )
 			}
 		},
-		setOverlay( type = '', props: AppView['overlay']['props'] = {} ) {
+		async setOverlay( type = '', props: AppView['overlay']['props'] = {} ) {
 			if( overlayTypes[type] ) {
+				// If we tried to set the overlay while another was active
+				if( this.overlay.active ) {
+					await this.unsetOverlay()
+				}
 				this.overlay.overlayType = type
 				this.overlay.props = props
 				this.overlay.active = true
@@ -50,10 +54,21 @@ export const useAppStore = defineStore( {
 				this.unsetOverlay()
 			}
 		},
-		unsetOverlay() {
+		/**
+		 * I know... I know... 'Why is this async' you ask. Something with how Pinia and/or
+		 * Vue work is making unsetOverlay() take a couple miliseconds to reset eveything.
+		 * As a result, in setOverlay(), I couldn't unset and then immediately set the overlay
+		 * again. Trying to set the props while they were being unset was causing them to stop
+		 * setting all together. Don't really know why that is, literally unset was like
+		 * "If I can't do my thing, then nobody can". So TLDR, this is async to force setOverlay
+		 * to wait for unsetOverlay to finish, otherwise unset throws a temper tantrum and I'm
+		 * not having any of it. I will turn this car around and _nobody_ gets Ice Cream!
+		 */
+		async unsetOverlay() {
 			this.overlay.active = false
 			this.overlay.overlayType = ''
 			this.overlay.props = {}
+			return true
 		},
 	},
 } )
