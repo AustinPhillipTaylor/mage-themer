b1c085394ed061d39b20f257082f225dc144b755
Illubyte <business.illubyte@gmail.com>
2021-11-22 04:41:24 -0500

    Added JSON-Schema (https://json-schema.org/) for interfaces.

--- src/ui/src/types/Palette.ts
+++ src/ui/src/types/Palette.ts
@@ -9,7 +9,7 @@ export interface PaletteColor {
 
 interface PaletteColors {
 	/** Each palette color is referenced by its GUID */
-	[guid: string]: PaletteColor
+	[key: string]: PaletteColor
 }
 
 export interface Palette {
@@ -23,5 +23,69 @@ export interface Palette {
 
 export interface Palettes {
 	/** Each palette is referenced by its GUID */
-	[guid: string]: Palette
+	[key: string]: Palette
+}
+
+export const paletteJSONSchema = {
+	'$schema': 'http://json-schema.org/draft-07/schema#',
+	'additionalProperties': {
+		'$ref': '#/definitions/Palette',
+	},
+	'definitions': {
+		'Palette': {
+			'properties': {
+				'colors': {
+					'$ref': '#/definitions/PaletteColors',
+					'description': 'Array of palette colors',
+				},
+				'guid': {
+					'description': 'Unique Identifier',
+					'type': 'string',
+				},
+				'name': {
+					'description': 'Display Name',
+					'type': 'string',
+				},
+			},
+			'type': 'object',
+		},
+		'PaletteColor': {
+			'properties': {
+				'guid': {
+					'description': 'Unique Identifier',
+					'type': 'string',
+				},
+				'name': {
+					'description': 'Display Name',
+					'type': 'string',
+				},
+				'rgb': {
+					'$ref': '#/definitions/RGB',
+					'description': 'RGB object representation of color. RGB values are values between 0 and 255',
+				},
+			},
+			'type': 'object',
+		},
+		'PaletteColors': {
+			'additionalProperties': {
+				'$ref': '#/definitions/PaletteColor',
+			},
+			'type': 'object',
+		},
+		'RGB': {
+			'properties': {
+				'b': {
+					'type': 'number',
+				},
+				'g': {
+					'type': 'number',
+				},
+				'r': {
+					'type': 'number',
+				},
+			},
+			'type': 'object',
+		},
+	},
+	'type': 'object',
 }
--- src/ui/src/types/Theme.ts
+++ src/ui/src/types/Theme.ts
@@ -35,3 +35,75 @@ export interface Themes {
 	/** Each theme is referenced by its GUID */
 	[key: string]: Theme
 }
+
+
+export const themeJSONSchema = {
+	'$schema': 'http://json-schema.org/draft-07/schema#',
+	'additionalProperties': {
+		'$ref': '#/definitions/Theme',
+	},
+	'definitions': {
+		'ColorVariation': {
+			'properties': {
+				'customNamingScheme': {
+					'description': 'If color variation should override theme naming scheme',
+					'type': 'boolean',
+				},
+				'guid': {
+					'description': 'Unique identifier',
+					'type': 'string',
+				},
+				'label': {
+					'description': 'Label identifier for color variation',
+					'type': 'string',
+				},
+				'mixingColor': {
+					'description': 'GUID of mixing color from mixing colors palette',
+					'type': 'string',
+				},
+				'namingScheme': {
+					'description': 'Variation-specific naming scheme',
+					'type': 'string',
+				},
+				'percentage': {
+					'description': 'Number 0-100, represents amount of mixingColor to add to base palette color',
+					'type': 'number',
+				},
+			},
+			'type': 'object',
+		},
+		'Theme': {
+			'properties': {
+				'guid': {
+					'description': 'Unique identifier',
+					'type': 'string',
+				},
+				'mixingColors': {
+					'description': 'GUID of mixing colors palette',
+					'type': 'string',
+				},
+				'name': {
+					'description': 'Display name',
+					'type': 'string',
+				},
+				'namingScheme': {
+					'description': 'String representing naming scheme for color variations. Includes\nspecial character codes that can be replaced with the variable information.',
+					'type': 'string',
+				},
+				'palette': {
+					'description': 'GUID of main color palette',
+					'type': 'string',
+				},
+				'variationMapping': {
+					'description': 'Array of variations for theme',
+					'items': {
+						'$ref': '#/definitions/ColorVariation',
+					},
+					'type': 'array',
+				},
+			},
+			'type': 'object',
+		},
+	},
+	'type': 'object',
+}
