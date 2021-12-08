f0f582429911ed55ab227e252a073db3250c9790
Illubyte <business.illubyte@gmail.com>
2021-12-07 23:13:37 -0500

    Removed obsolete utilities

--- src/ui/src/utils/nameParts.ts
+++
@@ -1,5 +0,0 @@
-// Utility to split styles names by folder delimiter
-export function nameParts( name: string ) {
-	const nameParts = name.split( "/" ).filter( ( part: string ) => !!part )
-	return nameParts.map( ( part: string ) => part.trim() )
-}
